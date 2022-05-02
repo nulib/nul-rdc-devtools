16e1ab7d33f6bea9f60edc0349c662d50625e20e
Michael B. Klein <mbklein@gmail.com>
2022-05-02 17:31:31 +0000

    Add SECRETS_PATH environment variable

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -4,4 +4,5 @@ export AWS_DEV_ENVIRONMENT=true
 export DEV_ENV=dev
 export DEV_PREFIX=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$instance_id" "Name=key,Values=Owner" | jq -r '.Tags[].Value')
 export ERL_AFLAGS="-kernel shell_history enabled"
+export SECRETS_PATH=dev-environment
 export PATH=$HOME/.nul-rdc-devtools/bin:$PATH
