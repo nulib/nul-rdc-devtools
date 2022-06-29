5936e05bc0be859651b73ec36979e8a5e336b2c2
Michael B. Klein <mbklein@gmail.com>
2022-06-29 15:52:19 +0000

    Add AWS_REGION and SECRET_KEY_BASE to environment

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -1,8 +1,10 @@
 instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 
 export AWS_DEV_ENVIRONMENT=true
+export AWS_REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
 export DEV_ENV=dev
 export DEV_PREFIX=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$instance_id" "Name=key,Values=Owner" | jq -r '.Tags[].Value')
 export ERL_AFLAGS="-kernel shell_history enabled"
+export SECRET_KEY_BASE=$(openssl rand -hex 32)
 export SECRETS_PATH=dev-environment/config
 export PATH=$HOME/.nul-rdc-devtools/bin:$PATH
