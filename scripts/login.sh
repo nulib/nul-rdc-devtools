88e1643427ca6c3e645c0c4320c1cf324ba9a28d
Michael B. Klein <mbklein@gmail.com>
2022-07-07 17:46:37 +0000

    Fix handling of AWS credentials file on login

--- scripts/login.sh
+++ scripts/login.sh
@@ -1,9 +1,10 @@
+if [[ -e ~/.aws/credentials ]] && grep 'if this file is modified' ~/.aws/credentials >/dev/null; then
+  echo -n "" > ~/.aws/credentials
+fi
+
 dir=$(dirname $0)
 source $dir/environment.sh
 source $dir/asdf-helpers.sh
 source $dir/terraform-helpers.sh
 
 . $HOME/.asdf/asdf.sh
-# if [[ -e ~/.aws/credentials ]]; then
-#   rm -f ~/.aws/credentials
-# fi
