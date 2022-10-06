602e4267dfe0e056e5a2c4d05b3cf2b2103290a1
Michael B. Klein <mbklein@gmail.com>
2022-10-06 10:02:25 -0500

    Make add_aws_adfs_profile.sh idempotent

--- scripts/add_aws_adfs_profile.sh
+++ scripts/add_aws_adfs_profile.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
-instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 profile=$1
 role_arn=$2
-net_id=$(aws ec2 describe-tags --filters Name=resource-id,Values=$instance_id --query "Tags[?Key=='NetID'].Value" --output text)
 
-cat >> ~/.aws/config <<__EOC__
+if ! grep "\[profile $profile\]" ~/.aws/config; then
+  region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
+  instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+  net_id=$(aws ec2 describe-tags --filters Name=resource-id,Values=$instance_id --query "Tags[?Key=='NetID'].Value" --output text)
+
+  cat >> ~/.aws/config <<__EOC__
 
 [profile $profile]
 region = $region
@@ -21,3 +23,4 @@ adfs_config.sspi = False
 adfs_config.u2f_trigger_default = True
 adfs_config.adfs_user = ads\\$net_id
 __EOC__
+fi
