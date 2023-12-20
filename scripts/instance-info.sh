4eddcc8fa3c6a38a41e2501449d171a77f914cb0
Michael B. Klein <mbklein@gmail.com>
2023-12-20 22:17:16 +0000

    instance-info.sh: Allow for spaces in tag values

--- scripts/instance-info.sh
+++ scripts/instance-info.sh
@@ -1,2 +1,2 @@
 INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-eval $(aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" | jq -r '.Tags[] | .["Key"] = (.Key | ascii_upcase | gsub("[^A-Z0-9_]"; "_")) | "INSTANCE_TAG_\(.Key)=\(.Value)"')
+eval $(aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" | jq -r '.Tags[] | .["Key"] = (.Key | ascii_upcase | gsub("[^A-Z0-9_]"; "_")) | "INSTANCE_TAG_\(.Key)=\"\(.Value)\""')
